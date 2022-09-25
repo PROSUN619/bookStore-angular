233c1a655b94dbb5de9dd925824095494434a7a2
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-09-25 15:45:16 +0530

    video 63: @viewchild. modify child from parent

--- bookstore/src/app/home/home.component.ts
+++ bookstore/src/app/home/home.component.ts
@@ -1,18 +1,39 @@
-import { Component, Inject, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, inject, OnInit, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { CounterService } from '../books/services/counter.service';
+import { AuthorComponent } from '../shared/components/author/author.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  //@ViewChild(AuthorComponent) //pass the component name
+  @ViewChild('author') //pass the template reference name
+  private authChild : AuthorComponent
+
+  @ViewChild('decbutton')
+  private decbutton : MatButton
+
+  @ViewChild('home')
+  private home : ElementRef
 
   constructor(public _counterService : CounterService, @Inject('app-title') public title : any) { }
 
   ngOnInit(): void {
 
   }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.authChild.setdata(100);  
+      this.decbutton.color = 'warn';    
+      this.home.nativeElement.innerHTML = 'Home Works Successfully'; 
+    }, 0);
+  }
+
 
   public increment() : void{
     this._counterService.incCounter();     
