3ebfbbb4834f30ba525aa3c421a95a6c0b6bba33
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-09-26 09:33:52 +0530

    video 67 : ngonchange

--- bookstore/src/app/home/home.component.ts
+++ bookstore/src/app/home/home.component.ts
@@ -1,52 +1,23 @@
-import { AfterViewInit, Component, ElementRef, Inject, inject, OnInit, ViewChild } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { CounterService } from '../books/services/counter.service';
-import { ExternalService } from '../books/services/external.service';
-import { AuthorComponent } from '../shared/components/author/author.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
-  //@ViewChild(AuthorComponent) //pass the component name
-  @ViewChild('author') //pass the template reference name
-  private authChild : AuthorComponent
 
-  @ViewChild('decbutton')
-  private decbutton : MatButton
-
-  @ViewChild('home')
-  private home : ElementRef
-
-  constructor(public _counterService : CounterService, @Inject('app-title') public title : any,
-              public  _externalService: ExternalService) { }
+constructor() { }
 
   ngOnInit(): void {
 
   }
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.authChild.setdata(100);  
-      this.decbutton.color = 'warn';    
-      this.home.nativeElement.innerHTML = 'Home Works Successfully'; 
-    }, 0);
-  }
+  public passData : string
 
-
-  public increment() : void{
-    this._counterService.incCounter();     
-  }
-
-  public decrement() : void{
-    this._counterService.decCounter(); 
-  }
-
-  public childData($event : any) : void {
-    console.log($event);
+  changeChildValue () : void {
+    this.passData = 'data passing from home'
   }
 
 }
--- bookstore/src/app/shared/components/author/author.component.ts
+++ bookstore/src/app/shared/components/author/author.component.ts
@@ -1,51 +1,27 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ExternalService } from 'src/app/books/services/external.service';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss']
 })
-export class AuthorComponent implements OnInit {
+export class AuthorComponent implements OnInit, OnChanges {
 
-  constructor(public _externalService : ExternalService) { }
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  @Input() 
+  public data : string 
+  
+  @Input() 
+  public dynamicdata : string
+ 
 
   ngOnInit(): void {
+
   }
-
-  public data : any
-
-  public setdata(v : any) {
-    this.data = v;
-  }
-  
-  //pass data from child to parent
-  @Output() mydata =  new EventEmitter<string>();
-  
-  passDatafromChildToParent() : void{
-    this.mydata.emit("this is author child data ");
-  }
-  
-  sharedService() : void{
-    this._externalService.sharedData = 'this is shared data from author component';
-  }
-
-
-  // @Input()
-  // data: any
-
-  // @Input()
-  // public get counter(): number {
-  //   return this._counter;
-  // }  
-
-  // public set counter(v : number) {
-  //   this._counter = ++v;
-  // }
-  
-  // private _counter: number
-
-  // @Input()
-  // visible: boolean
 
 }
