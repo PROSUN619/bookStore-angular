45463e9062dc216147da3b6da1eb17d142783cef
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-09-27 21:49:38 +0530

    vidoe 73 : ngdestroy

--- bookstore/src/app/home/home.component.ts
+++ bookstore/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AuthorComponent } from '../shared/components/author/author.component';
 import { AuthorModel } from '../shared/models/author.model';
 
@@ -7,35 +7,40 @@ import { AuthorModel } from '../shared/models/author.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class HomeComponent implements OnInit, AfterViewInit, AfterViewChecked, OnDestroy {
 
   public obj: AuthorModel = { id: '123', name: 'Prasun' };
   public address: string = 'India'
-  private count: number = 0;
+  public count: number = 0;
   public passData: string
+  private time : any
 
-  @ViewChild('btnChange') btnChange : ElementRef 
-  @ViewChild(AuthorComponent) authComp : AuthorComponent
+
+  @ViewChild('btnChange') btnChange: ElementRef
+  @ViewChild(AuthorComponent) authComp: AuthorComponent
 
   constructor() {
     //console.log('Home Component constructor');
-    
+
+  }
+  
+  ngOnDestroy(): void {
+    clearInterval(this.time);
   }
 
-    
   ngOnInit(): void {
-    console.log('btnChange',this.btnChange);    
+    console.log('btnChange', this.btnChange);
+    this.timer();
   }
 
   ngAfterViewInit(): void {
-    console.log('btnChange',this.btnChange);    
+    console.log('btnChange', this.btnChange);
     this.btnChange.nativeElement.innerHTML = 'button name changed from ngAfterViewInit';
   }
 
   ngAfterViewChecked(): void {
-    console.log('Author Component Value changed ' + this.authComp.count);    
+    console.log('Author Component Value changed ' + this.authComp.count);
   }
-
 
   changeChildValue(): void {
     this.passData = 'data passing from home'
@@ -46,4 +51,12 @@ export class HomeComponent implements OnInit, AfterViewInit, AfterViewChecked {
     this.count++;
     this.address += this.count;
   }
+
+  timer(): void {
+   this.time =  setInterval(() => {
+      this.count++;
+      console.log(this.count);
+    },1000);
+  }
+
 }
